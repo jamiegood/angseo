79a71d25735990dff8b4bebfeb4dcb1d7e521979
Good, Jamie <Jamie.Good@paddypower.com>
2014-06-19 19:48:18 +0100

    Added node static server

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-// web.js
-var express = require("express");
-var logfmt = require("logfmt");
+var express = require('express');
 var app = express();
 
-app.use(logfmt.requestLogger());
+app.use(express.static(__dirname + '/'));
 
-app.get('/', function(req, res) {
-    res.send('Hello World!');
-});
-
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-    console.log("Listening on " + port);
-});
+app.listen(process.env.PORT || 3000);
