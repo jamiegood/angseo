660229dece8a950f816a54043b150d059eb9bdfd
Good, Jamie <Jamie.Good@paddypower.com>
2014-06-25 11:31:04 +0100

    updating links to remove hasbang

--- app/js/app.js
+++ app/js/app.js
@@ -15,20 +15,21 @@ var phonecatApp = angular.module('phonecatApp', [
 phonecatApp.config(['$routeProvider', '$locationProvider',
   function($routeProvider, $locationProvider) {
 
-      $locationProvider.hashPrefix('!');
+      $locationProvider.html5Mode(true);
+      //$locationProvider.hashPrefix('!');
 
 
     $routeProvider.
-      when('/phones', {
-        templateUrl: 'partials/phone-list.html',
+      when('/app/phones', {
+        templateUrl: '/app/partials/phone-list.html',
         controller: 'PhoneListCtrl'
       }).
-      when('/phones/:phoneId', {
-        templateUrl: 'partials/phone-detail.html',
+      when('/app/phones/:phoneId', {
+        templateUrl: '/app/partials/phone-detail.html',
         controller: 'PhoneDetailCtrl'
       }).
       otherwise({
-        redirectTo: '/phones'
+        redirectTo: '/app/phones'
       });
 
       //$locationProvider.hasPrefix('!');
